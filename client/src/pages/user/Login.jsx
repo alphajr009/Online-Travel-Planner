bfce506c26ec3819ca2ace0ada882e4df340533b
Nandana <50739907+NandanaX@users.noreply.github.com>
2023-07-05 09:22:27 +0530

    Nav bar add to home page

--- client/src/pages/user/Login.jsx
+++ client/src/pages/user/Login.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import { Layout, Space, Col, Row, Button, Form, Input, Checkbox } from "antd";
 
 import "../../css/login.css";
---
+++ client/src/pages/user/home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navbar from '../../components/navbar/MainNavbar'
+
+function Home() {
+    return (
+      <div className="Home">
+            <Navbar></Navbar>
+      </div>
+    );
+  }
+  
+  export default Home;
