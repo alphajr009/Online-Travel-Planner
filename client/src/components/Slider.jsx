89f9a1e5a33960ba8361187dfd48eb4c1c55fe0b
Nandana <50739907+NandanaX@users.noreply.github.com>
2023-07-05 14:04:49 +0530

    home page location card structure build

--- client/src/components/Slider.jsx
+++ client/src/components/Slider.jsx
@@ -10,7 +10,7 @@ const contentStyle = {
 const Slider = () => (
   <Carousel autoplay>
     <div>
-      <h3 style={contentStyle}>1</h3>
+    <h3 style={contentStyle}>2</h3>
     </div>
     <div>
       <h3 style={contentStyle}>2</h3>
