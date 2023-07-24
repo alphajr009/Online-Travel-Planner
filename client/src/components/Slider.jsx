0db0badf973a07814f37fccf5e3fc3475abd22eb
Pasindu Randika <99603230+alphajr009@users.noreply.github.com>
2023-07-25 00:42:08 +0530

    Update Explore

--- client/src/components/Slider.jsx
+++ client/src/components/Slider.jsx
@@ -1,34 +1,45 @@
+import { useState } from "react";
 import { Button, Carousel, Col } from "antd";
 import { Input } from "antd";
+import { useNavigate } from "react-router-dom";
 
 import "../css/slider.css";
-const contentStyle = {
-	height: "450px",
-	color: "#fff",
-	lineHeight: "160px",
-	textAlign: "center",
-	background: "#364d79",
-	margin: "0px",
+
+const Slider = () => {
+	const navigate = useNavigate();
+	const [searchValue, setSearchValue] = useState("");
+
+	const handleSearch = () => {
+		navigate(`/plantrip`, { state: { search: searchValue } });
+	};
+
+	return (
+		<Carousel autoplay>
+			<div className="slider-images-01">
+				<Col className="slider-content" span={8}>
+					<h1 className="text-align-center">Make Your Dream Trip </h1>
+					<p>
+						Discover the best places to visit, eat, and stay in any location. Explore exciting activities,
+						indulge in delicious cuisine, and book accommodations that suit your style.
+						Create unforgettable memories with ease, all in one place
+					</p>
+
+					<div className="search-bar">
+						<Input
+							className="search-input"
+							placeholder="Where you want to go ?"
+							value={searchValue}
+							onChange={(e) => setSearchValue(e.target.value)}
+							onPressEnter={handleSearch}
+						/>
+						<Button type="primary" size="large" onClick={handleSearch}>
+							Search
+						</Button>
+					</div>
+				</Col>
+			</div>
+		</Carousel>
+	);
 };
-const Slider = () => (
-	<Carousel autoplay>
-		<div className="slider-images-01">
-			<Col className="slider-content" span={8}>
-				<h1 className="text-align-center ">Make Your Dream Trip </h1>
-				<p>
-					{" "}
-					Discover the best places to visit, eat, and stay in any location. Explore exciting activities,
-					indulge in delicious cuisine, and book accommodations that suit your style.
-					Create unforgettable memories with ease, all in one place
-				</p>
 
-				 <div class="search-bar">
-					<Input className="search-input" placeholder="Where you want to go ?" />
-					<Button type="primary" size="large">Search</Button>
-				 </div>
-
-			</Col>
-		</div>
-	</Carousel>
-);
 export default Slider;
--- client/src/pages/PlanTrip.jsx
+++ client/src/pages/PlanTrip.jsx
@@ -1,63 +1,119 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "../components/navbar/MainNavbar";
-import Slider01 from "../components/Slider";
 import Slider from "react-slick";
+import Slider01 from "../components/Slider";
 import "../css/palnTrip.css";
 import { Carousel, Card, Col } from "antd";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import PlaceCard from '../components/PlaceCard'
 import Sigiriya from '../assets/sigiriya.png'
 import Mirissa from '../assets/mirissa.png'
 import Ella from '../assets/ella.png'
 import Kandy from '../assets/kandy.png'
 import Yala from '../assets/yala.png'
 import Colombo from '../assets/colombo.png'
+import { useLocation } from "react-router-dom";
+import axios from "axios";
 
 const contentStyle = {
 	background: "red",
 };
 const { Meta } = Card;
+
+
 function PlanTrip() {
+
+	const PlaceCard = ({ PlaceCardName, src }) => (
+		<Card
+			hoverable
+			cover={<img alt="example" src={src} />}
+		>
+			<p>{PlaceCardName}</p>
+		</Card>
+	);
+
+
+	const [doplace, setDo] = useState([]);
+	const [eatplace, setEat] = useState([]);
+	const [stayplace, setStay] = useState([]);
+	const [filteredPlaces, setFilteredPlaces] = useState([]);
+	const [isLoading, setIsLoading] = useState(true);
+	const [showSpinner, setShowSpinner] = useState(true);
+
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				const response = await axios.get('/api/places/getallplaces');
+				const data = response.data;
+				const doPlaces = data.places.filter(place => place.category === 'Do');
+				const eatPlaces = data.places.filter(place => place.category === 'Eat');
+				const stayPlaces = data.places.filter(place => place.category === 'Stay');
+
+				setDo(doPlaces);
+				setEat(eatPlaces);
+				setStay(stayPlaces);
+
+				setTimeout(() => {
+					setIsLoading(false);
+					setShowSpinner(false);
+				}, 1500);
+			} catch (error) {
+				console.log(error);
+				setIsLoading(false);
+				setShowSpinner(false);
+			}
+		};
+
+		fetchData();
+	}, []);
+
+
+	const location = useLocation();
+	const searchValue = location.state?.search || "";
+	console.log(searchValue)
+
+
 	var settings = {
-        infinite: false,
-        speed: 500,
-        slidesToShow: 4,
-        slidesToScroll: 1,
-        initialSlide: 0,
-        responsive: [
-            {
-                breakpoint: 1024,
-                settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 3,
-                    infinite: true,
-                    dots: true
-                }
-            },
-            {
-                breakpoint: 600,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 2,
-                    initialSlide: 2
-                }
-            },
-            {
-                breakpoint: 480,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1
-                }
-            }
-        ]
-    };
+		infinite: false,
+		speed: 500,
+		slidesToShow: 4,
+		slidesToScroll: 1,
+		initialSlide: 0,
+		responsive: [
+			{
+				breakpoint: 1024,
+				settings: {
+					slidesToShow: 3,
+					slidesToScroll: 3,
+					infinite: true,
+					dots: true
+				}
+			},
+			{
+				breakpoint: 600,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 2,
+					initialSlide: 2
+				}
+			},
+			{
+				breakpoint: 480,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1
+				}
+			}
+		]
+	};
+
+
 	return (
 		<div className="Home">
 			<Navbar></Navbar>
 			<Slider01></Slider01>
-			<div className="container">
-				<h1>Hello World</h1>
+			<div className="container plantrip">
+				<h3>Do</h3>
 				<Slider {...settings}>
 					<Col className="location-card">
 						<PlaceCard PlaceCardName="Colombo" src={Colombo} />
