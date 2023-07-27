557675e72ee0e69fab6dccc577e4959ba6f93c04
Pasindu Randika <99603230+alphajr009@users.noreply.github.com>
2023-07-28 02:09:21 +0530

    Trip Complete

--- routes/tripsRoute.js
+++ routes/tripsRoute.js
@@ -52,4 +52,69 @@ router.post("/createtrip", upload.single("image"), async (req, res) => {
 });
 
 
+router.get("/getalltrips", async (req, res) => {
+    const userId = req.query.userId;
+
+    try {
+        const trips = await Trip.find({ userid: userId });
+
+        if (!trips || trips.length === 0) {
+            return res.status(404).json({ message: "No trips found for the given userId." });
+        }
+
+        res.status(200).json(trips);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Internal server error" });
+    }
+});
+
+
+router.post('/deletetrip', async (req, res) => {
+
+    const { _id } = req.body;
+
+    try {
+
+        const trip = await Trip.findByIdAndRemove(_id);
+
+        if (!trip) return res.status(404).send('Trip not found');
+
+        const imagePath = `uploads/${trip._id}.jpg`;
+        if (fs.existsSync(imagePath)) {
+            fs.unlinkSync(imagePath);
+        }
+
+        res.send('Trip deleted successfully');
+
+    } catch (error) {
+        console.log(error);
+        res.status(400).send('Error deleting Place');
+    }
+
+});
+
+
+router.post("/gettripbyid", async (req, res) => {
+
+
+    const tripid = req.body.tripid
+
+    try {
+        const trip = await Trip.find({ _id: tripid })
+
+        return res.json({ trip })
+    } catch (error) {
+        return res.status(400).json({ message: error })
+    }
+
+
+});
+
+
+
+
+
+
+
 module.exports = router
