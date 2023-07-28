c15eda0c558f15b82a6c4fcaf20aaa4380c10b1b
Pasindu Randika <99603230+alphajr009@users.noreply.github.com>
2023-07-29 00:14:30 +0530

    Deploy

--- server.js
+++ server.js
@@ -37,8 +37,14 @@ app.use('/api/trips', tripRoute)
 const port = process.env.PORT || 5000;
 
 
-if (process.env.NODE_ENV == 'production') {
-    app.use(express.static('client/build'))
+if (process.env.NODE_ENV === "production") {
+    // Set the static folder
+    app.use(express.static("client/build"));
+
+    // Serve the index.html file for all non-API routes
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    });
 }
 
 app.listen(port, () => console.log('Node Server Started using Nodemon!'));
